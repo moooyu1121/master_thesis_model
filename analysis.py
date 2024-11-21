6b95787fee336636b66a384986f58561ae39b76e
moooyu1121 <ys11.morita@gmail.com>
2024-11-21 20:00:44 +0900

    adjusting analysis

--- analysis.py
+++ analysis.py
@@ -1672,6 +1672,7 @@ def net_cost_by_battery_ev_pv_dr_exist_plot_2(thread_num, folder_path, include_c
     ax.set_title('Net Electricity Cost Distribution')
     ax.yaxis.grid(True)
     ax.set_axisbelow(True)
+    ax.set_ylim(-4000, 14000)
 
     # Adding mean and standard deviation text
     for i in range(len(categories)):
